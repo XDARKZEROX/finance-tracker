786b0f2dc1bb57951522cceb52095c94e8d2e1b1
Alexander Guzman <nocturnal.classical@hotmail.com>
2018-10-17 22:07:54 -0500

    added stock controller and my_portofolio view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,14 @@
+#model
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
     price = strip_commas(looked_up_stock.latest_price)
-    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
+    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price) 
+  rescue => exception
+    return nil
   end
 
   def self.strip_commas(number)
-    number.to_s.gsub(",","")
+    number.to_s.gsub(',', '')
   end
 end
